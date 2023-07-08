79d9d7c14b8280855c5ddec6136d12815e5eb7c2
shrookayman <shrookayman617@gmail.com>
2023-07-08 17:58:28 +0300

    debounce function

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
-let resultsContainer = document.getElementsByClassName("container")[0]
+let resultsContainer = document.getElementsByClassName("container")[0];
 
-const validateInput = (el) => {
-    if(el.value === ""){
-        resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-    }else{
-        generateResults(el.value, el)
-    }
-}
+
+const debounce = (func, delay) => {
+  let timerId;
+
+  return function() {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, arguments);
+    }, delay);
+  };
+};
+
+
+const validateInput = debounce(function(el) {
+  if (el.value === "") {
+    resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
+  } else {
+    generateResults(el.value, el);
+  }
+}, 300); // Adjust the debounce delay as needed (e.g., 300 milliseconds)
 
 const generateResults = (searchValue, inputField) => {
-    fetch(
-        "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
-        + searchValue
-    )
+  fetch(
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=" +
+      searchValue
+  )
     .then(response => response.json())
     .then(data => {
-        let results = data.query.search
-        let numberOfResults = data.query.search.length
-        resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
-            let result = document.createElement("div")
-            result.classList.add("results")
-            result.innerHTML = `
-            <div>
-                <h3>${results[i].title}</h3>
-                <p>${results[i].snippet}</p>
-            </div>
-            <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
-            `
-            resultsContainer.appendChild(result)
-        }
-        if(inputField.value === ""){
-            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-        }
-    })
-}
+      let results = data.query.search;
+      let numberOfResults = data.query.search.length;
+      resultsContainer.innerHTML = "";
+      for (let i = 0; i < numberOfResults; i++) {
+        let result = document.createElement("div");
+        result.classList.add("results");
+        result.innerHTML = `
+          <div>
+            <h3>${results[i].title}</h3>
+            <p>${results[i].snippet}</p>
+          </div>
+          <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
+        `;
+        resultsContainer.appendChild(result);
+      }
+      if (inputField.value === "") {
+        resultsContainer.innerHTML = "<p>Type something in the above search input</p>";
+      }
+    });
+};
+
+// Attach event listener to search input
+const searchInput = document.getElementById("search");
+searchInput.addEventListener("keyup", function() {
+  validateInput(this);
+});
